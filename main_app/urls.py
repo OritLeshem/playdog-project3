3f371779ca4a982611809402de6909ebcbce24c1
OritLeshem <azmono@gmail.com>
2021-05-22 08:38:47 -0400

    crud event

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
     path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
     path('dogs/<int:pk>/delete/', views.DeleteDog, name='dogs_delete'),
-
+    path('events/',views.events_index, name="events_index"),
+    path('events/<int:event_id>/', views.events_detail, name='events_detail'),
+    path('events/create/', views.EventCreate.as_view(), name='events_create'),
+    path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
+    path('events/<int:pk>/delete/', views.DeleteEvent, name='events_delete'),
    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,26 +7,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class DogCreate(CreateView):
-  model = Dog
-  fields = ['name','breed', 'description', 'age']
-  def form_valid(self, form):
-    form.instance.user = self.request.user  # form.instance is the cat
-    return super().form_valid(form)
-  # success_url = '/dogs/'
-class DogUpdate(UpdateView):
-  model = Dog
-  # Let's disallow the renaming of a cat by excluding the name field!
-  fields = ['breed', 'description', 'age']
-
-def DeleteDog(request, pk):
-  dog=Dog.objects.get(id=pk)
-  dog.delete()
-  return redirect('dogs_index')
- 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
+# DOGS:_________________
 def dogs_index(request):
   dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
@@ -36,4 +20,50 @@ def dogs_detail(request, dog_id):
   # events_dog_dosnt_have=Event.objects.exclude(id__in = dog.events.all().values_list('id'))
   # events=Event.objects.all()
   return render(request, 'dogs/detail.html', { 'dog': dog})  
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name','breed', 'description', 'age']
+  def form_valid(self, form):
+    form.instance.user = self.request.user  
+    return super().form_valid(form)
+  
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+def DeleteDog(request, pk):
+  dog=Dog.objects.get(id=pk)
+  dog.delete()
+  return redirect('dogs_index')
+
+
+# EVENTS:_________________
+class EventCreate(CreateView):
+  model = Event
+  fields = ['name','description', 'date', 'location']
+  def form_valid(self, form):
+    form.instance.user = self.request.user  
+    return super().form_valid(form)
+  success_url = '/events/'  
+
+
+def events_index(request):
+  events = Event.objects.all()
+  return render(request, 'events/events_index.html', {'events': events})
+
+def events_detail(request, event_id):
+  event = Event.objects.get(id=event_id)
+  return render(request, 'events/events_detail.html', { 'event': event})
+
+class EventUpdate(UpdateView):
+  model = Event
+  fields = ['name', 'description']
+  success_url = '/events/'  
+  
+
+def DeleteEvent(request, pk):
+  event=Event.objects.get(id=pk)
+  event.delete()
+  return redirect('events_index')    
 
