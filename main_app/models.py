cfcfa5864987d4d6d4ff2c7146b0f327c39ac844
Ben Shekhtman <benshekhtman@hotmail.com>
2021-05-23 01:32:11 -0400

    styling updates

--- main_app/models.py
+++ main_app/models.py
@@ -19,20 +19,21 @@ SIZES = (
     ('X', 'EXTRA LARGE')
 )
 
+
 class Dog(models.Model):
-    name=models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     size = models.CharField(
         max_length=1,
-            choices=SIZES,
-            default=SIZES[2][0]
+        choices=SIZES,
+        default=SIZES[2][0]
     )
     gender = models.CharField(
         max_length=1,
-            choices=GENDERS,
-            default=GENDERS[0][0]
+        choices=GENDERS,
+        default=GENDERS[0][0]
     )
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = CharField(max_length=250)
@@ -51,7 +52,7 @@ class Event(models.Model):
     description = models.TextField(max_length=250)
     location = models.FloatField()
     attendees = models.ManyToManyField(Dog)
-    
+    image = models.ImageField(blank=True, null=True, upload_to='images/')
+
     def __str__(self):
         return self.name
-  
