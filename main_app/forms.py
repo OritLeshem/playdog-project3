efaae3a538e9f6715f141834bc231b8361658273
Ben Shekhtman <benshekhtman@hotmail.com>
2021-05-23 13:32:17 -0400

    finished form styling

--- main_app/forms.py
+++ main_app/forms.py
@@ -6,4 +6,4 @@ from .models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['name', 'date', 'description', 'location', 'image']
+        fields = ['name', 'description', 'date', 'location', 'image']
