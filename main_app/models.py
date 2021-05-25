60b7a0567a5eb641b0434d58cf3a2bdd0b2d951b
Ben Shekhtman <benshekhtman@hotmail.com>
2021-05-24 20:37:24 -0400

    morestyling branch

--- main_app/models.py
+++ main_app/models.py
@@ -19,23 +19,23 @@ SIZES = (
     ('X', 'EXTRA LARGE')
 )
 
+
 class Dog(models.Model):
-    name=models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     size = models.CharField(
         max_length=1,
-            choices=SIZES,
-            default=SIZES[2][0]
+        choices=SIZES,
+        default=SIZES[2][0]
     )
     gender = models.CharField(
         max_length=1,
-            choices=GENDERS,
-            default=GENDERS[0][0]
+        choices=GENDERS,
+        default=GENDERS[0][0]
     )
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = CharField(max_length=250)
+    image = models.ImageField(blank=True, null=True, upload_to='images/')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -44,6 +44,9 @@ class Dog(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
 
+    class Meta:
+        ordering = ['name']
+
 
 class Event(models.Model):
     name = models.CharField(max_length=50)
@@ -51,7 +54,11 @@ class Event(models.Model):
     description = models.TextField(max_length=250)
     location = models.FloatField()
     attendees = models.ManyToManyField(Dog)
-    
+    image = models.ImageField(blank=True, null=True, upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-  
+
+    class Meta:
+        ordering = ['date']
